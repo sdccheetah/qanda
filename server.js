7577d6d6829735a2074d265eaf95ea4130545728
Kate <krharney@gmail.com>
2019-09-24 19:50:10 -0400

    More server fixing! Using mongoose model to enter new questions now.

--- server.js
+++ server.js
@@ -2,52 +2,269 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const model = require("./model.js");
 
 app.use(bodyParser.json());
 
-var MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/questions", function(
-  err,
-  client
-) {
-  if (err) throw err;
+MongoClient.connect(
+  "mongodb://localhost:27017/questions2",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function(err, client) {
+    if (err) throw err;
 
-  var db = client.db("questions");
+    const db = client.db("questions2");
 
-  app.get("/qa/:product_id", (req, res) => {
-    console.log(req.params);
-
-    // should the promises be nested or one after another,
-    // pushing results array into the data returned by the previous?
-    db.collection("questions")
-      .find({ product_id: Number(req.params.product_id) })
-      .forEach(question => {
-        console.log("QUESTION DATA: ", question);
-        db.collection("answers")
-          .find({ question_id: Number(question.id) })
-          .forEach(answer => {
-            db.collection("photos")
-              .find({ answer_id: answer.id }, { url: 1, _id: 0 })
-              .toArray((err, result) => {
-                if (err) throw err;
-                answer["photos"] = result;
-                console.log(answer);
+    app.get("/qa/:product_id", (req, res) => {
+      let returnObj = {};
+      returnObj["product_id"] = req.params.product_id;
+      db.collection("questions")
+        .find({ product_id: Number(req.params.product_id) })
+        .project({
+          _id: 0,
+          question_id: 1,
+          question_body: 1,
+          question_date: 1,
+          asker_name: 1,
+          question_helpfulness: 1,
+          reported: 1
+        })
+        .toArray()
+        .then(questions => {
+          returnObj["results"] = questions;
+          console.log(returnObj);
+          let innerPromises;
+          let promises = returnObj["results"].map(question => {
+            let questionId = Number(question.question_id);
+            return db
+              .collection("answers")
+              .find({ question_id: questionId })
+              .project({
+                _id: 0,
+                id: 1,
+                body: 1,
+                date: 1,
+                answerer_name: 1,
+                helpfulness: 1
+              })
+              .toArray()
+              .then(answers => {
+                question["answers"] = {};
+                innerPromises = answers.map(answer => {
+                  return db
+                    .collection("photos")
+                    .find({ answer_id: answer.id })
+                    .project({
+                      _id: 0,
+                      url: 1
+                    })
+                    .toArray()
+                    .then(photos => {
+                      photos = photos.map(photo => photo.url);
+                      question["answers"][answer.id] = answer;
+                      question["answers"][answer.id].photos = photos;
+                    })
+                    .catch(err => console.log(err));
+                });
+                return Promise.all(innerPromises);
+              })
+              .catch(err => {
+                console.log(err);
               });
-          })
-          .toArray((err, result) => {
-            if (err) throw err;
-            question["answers"] = result;
           });
-      });
+          Promise.all(promises).then(() => {
+            res.send(returnObj);
+          });
+        })
+        .catch(err => console.log(err));
+    });
 
-    // .toArray(function(err, result) {
-    //   if (err) throw err;
-    //   console.log(result);
-    //   result.map(()=>{})
-    //   res.send(result);
-    // });
-  });
-});
+    app.get("/qa/:question_id/answers", (req, res) => {
+      let page = Number(req.query.page) || 0;
+      let count = Number(req.query.count) || 5;
+      let question_id = req.params.question_id;
+      let returnObj = {};
+      returnObj["question"] = question_id;
+      returnObj["page"] = page;
+      returnObj["count"] = count;
+      db.collection("answers")
+        .find({ question_id: Number(question_id) })
+        .project({
+          _id: 0,
+          id: 1,
+          body: 1,
+          date: 1,
+          answerer_name: 1,
+          helpfulness: 1
+        })
+        .limit(count)
+        .skip(page * count)
+        .toArray()
+        .then(answers => {
+          returnObj["results"] = answers.map(answer => {
+            answer["answer_id"] = answer["id"];
+            delete answer["id"];
+            return answer;
+          });
+          innerPromises = answers.map(answer => {
+            return db
+              .collection("photos")
+              .find({ answer_id: answer.answer_id })
+              .toArray()
+              .then(photos => {
+                answer["photos"] = photos.map(photo => photo.url);
+              })
+              .catch(err => console.log(err));
+          });
+          return Promise.all(innerPromises);
+        })
+        .then(data => {
+          res.send(returnObj);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    app.post("/qa/:product_id", (req, res) => {
+      db.collection("questions")
+        .count()
+        .then(count => {
+          model.Question.create(
+            {
+              question_id: count + 1,
+              product_id: Number(req.params.product_id),
+              question_body: req.body.body,
+              question_date: new Date(),
+              asker_name: req.body.name,
+              asker_email: req.body.email,
+              reported: 0,
+              question_helpfulness: 0
+            },
+            function(err, data) {
+              if (err) console.log(err);
+              else res.sendStatus(204);
+            }
+          );
+        })
+        .catch(err => {
+          console.log("error: ", err);
+          res.sendStatus(500);
+        });
+    });
+
+    app.post("/qa/:question_id/answers", (req, res) => {
+      db.collection("answers")
+        .count()
+        .then(count => {
+          db.collection("answers")
+            .insertOne({
+              id: count + 1,
+              question_id: Number(req.params.question_id),
+              body: req.body.body,
+              date_written: new Date(),
+              answerer_name: req.body.name,
+              answerer_email: req.body.email,
+              reported: 0,
+              helpful: 0
+            })
+            .then(data => {
+              let promises = [];
+              req.body.photos.forEach(photo => {
+                promises.push(
+                  db.collection("photos").insertOne({
+                    answer_id: count + 1,
+                    url: photo
+                  })
+                );
+              });
+              Promise.all(promises).then(res.sendStatus(201));
+            })
+            .catch(err => {
+              console.log(err);
+              res.sendStatus(500);
+            });
+        })
+        .catch(err => {
+          console.log("error: ", err);
+          res.sendStatus(500);
+        });
+    });
+    app.put("/qa/question/:question_id/helpful", (req, res) => {
+      db.collection("questions")
+        .updateOne(
+          { id: Number(req.params.question_id) },
+          { $inc: { helpful: 1 } }
+        )
+        .then(data => {
+          // why does it not send this status??
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    });
+    app.put("/qa/question/:question_id/report", (req, res) => {
+      db.collection("questions")
+        .updateOne(
+          { id: Number(req.params.question_id) },
+          { $inc: { reported: 1 } }
+        )
+        .then(data => {
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    });
+    app.put("/qa/answer/:answer_id/helpful", (req, res) => {
+      db.collection("answers")
+        .updateOne(
+          { id: Number(req.params.answer_id) },
+          { $inc: { helpful: 1 } }
+        )
+        .then(data => {
+          // why does it not send this status??
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    });
+    app.put("/qa/answer/:answer_id/report", (req, res) => {
+      db.collection("answers")
+        .updateOne(
+          { id: Number(req.params.answer_id) },
+          { $inc: { reported: 1 } }
+        )
+        .then(data => {
+          // why does it not send this status??
+          res.sendStatus(204);
+        })
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    });
+  }
+);
+
+cleanUpData = obj => {
+  for (let i = 0; i < obj.results.length; i++) {
+    let question = obj.results[i];
+    if (question.reported === 1) obj.results.splice(i, 1);
+    else {
+      delete question["_id"];
+      delete question["__v"];
+      question["question_id"] = question["id"];
+      delete question["id"];
+    }
+  }
+  return obj;
+};
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
